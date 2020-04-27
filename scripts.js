b6d2661488b2e76bd56b9810c3b5c49ab0ed527a
an4morph <avgustin456@gmail.com>
2020-04-27 18:34:27 +0600

    Add comments

--- scripts.js
+++ scripts.js
@@ -1,31 +1,21 @@
+// метод облегчающий создание html элемента
 const createEl = (tag, text, attrs = {}) => {
   const el = document.createElement(tag)
   el.textContent = text
+  // цикл по атрибутам из агрументов функции и их назначение
   Object.keys(attrs).forEach((key) => {
     el.setAttribute(key, attrs[key])
   })
   return el
 }
 
+// GET '/list' - запрос на получение всего списка задач
 const fetchGetTaskList = () => {
   return fetch(`${endpoint}/list`)
     .then(response => response.json())
 }
 
-const fetchEditTask = (id, body) => {
-  return fetch(`${endpoint}/edit/${id}`, {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(body),
-  })
-}
-
-const fetchDeleteTask = (id) => {
-  return fetch(`${endpoint}/delete/${id}`, {
-    method: 'DELETE',
-  })
-}
-
+// POST '/add' - запрос на создание задачи по ID
 const fetchAddTask = (body) => {
   return fetch(`${endpoint}/add`, {
     method: 'POST',
@@ -34,23 +24,49 @@ const fetchAddTask = (body) => {
   })
 }
 
+// PUT '/edit/:id' - запрос на редактирование задачи по ID
+const fetchEditTask = (id, body) => {
+  return fetch(`${endpoint}/edit/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body),
+  })
+}
+
+// DELETE '/delete/:id' - запрос на удаление задачи по ID
+const fetchDeleteTask = (id) => {
+  return fetch(`${endpoint}/delete/${id}`, {
+    method: 'DELETE',
+  })
+}
+
 const endpoint = 'http://localhost:3000'
 
+// функция, которая отрисовывает html задачу. 
+// Аргументы task - объект задачи с бэка, list - родительский DOM элемент, в котором отрисуются задачи
 const renderTask = (task, list) => {
+  // создание основных html элементов в задаче
   const li = createEl('li')
   const text = createEl('div', task.text, { class: task.done ? 'text done' : 'text' })
   const btnWrapper = createEl('div', null, { class: 'btn-wrapper' })
 
+  // переменная отображающая что можно сделать с задачей
+  // если задача не сделана, то надпись на кнопке 'Сделано' пометит ее как сделанную и наоборот
   const doneBtnText = !task.done ? 'Сделано' : 'Не сделано'
+
+  // создание html елементов кнопок задачи
   const doneBtn = createEl('button', doneBtnText)
   const editBtn = createEl('button', ' Редактировать')
   const deleteBtn = createEl('button', 'Удалить')
   
+  // логика клика по кнопке "Удалить"
   deleteBtn.addEventListener('click', () => {
+    // запрос на бэкенд DELETE /delete/:id
     fetchDeleteTask(task.id)
+    // при успешном ответе страница перезагружается
     .then(() => window.location.reload())
   })
-
+  // логика клика по кнопке "Редактировать"
   editBtn.addEventListener('click', () => {
     const input = createEl('input', { class: 'edit-input' })
     input.type = 'text'
@@ -59,16 +75,21 @@ const renderTask = (task, list) => {
     li.insertBefore(input, text)
     li.removeChild(text)
     input.addEventListener('blur', () => {
+      // запрос на бэкенд PUT /edit/:id
       fetchEditTask(task.id, { text: input.value })
+      // при успешном ответе страница перезагружается
         .then(() => window.location.reload())
     })
   })
-
+  // логика клика по кнопке "Сделано"
   doneBtn.addEventListener('click', () => {
+    // запрос на бэкенд PUT /edit/:id
     fetchEditTask(task.id, { done: !task.done })
+    // при успешном ответе страница перезагружается
     .then(() => window.location.reload())
   })
 
+  // прикрепление html элементов к DOM дереву
   li.appendChild(text)
   li.appendChild(btnWrapper)
   btnWrapper.appendChild(doneBtn)
@@ -77,22 +98,31 @@ const renderTask = (task, list) => {
   list.appendChild(li)
 }
 
+// функция, которая отрисовывает весь список задач на экран
 const renderTaskList = () => {
   const list = createEl('ul', null, { id: 'list' })
   document.body.appendChild(list)
 
+  // запрос на бэкенд GET /list
   fetchGetTaskList()
+    // в результате успешного ответа: поочередно отрисовывается каждая задача в списке
     .then(taskList => taskList.forEach((item) => renderTask(item, list)))
 }
 
+// вызываем функцию отрисовки списка задач при загрузке страницы
 renderTaskList()
 
+
+// создание новой задачи //
+// // // // // // // // /// 
 const input = document.querySelector('input[name="todo-text"]')
 const textarea = document.querySelector('textarea[name="todo-description"]')
 const createBtn = document.querySelector('#create')
 
 createBtn.addEventListener('click', () => {
+  // запрос на бэкенд POST /add
   fetchAddTask({ text: input.value, textarea: input.value })
+    // при успешном ответе страница перезагружается
     .then(() => window.location.reload())
 })
 
