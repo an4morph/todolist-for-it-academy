e6f213b58f401a316577712f454eb880a9f4ff81
an4morph <avgustin456@gmail.com>
2020-04-29 18:48:02 +0600

    change error text

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ const createEl = (tag, text, attrs = {}) => {
 const fetchGetTaskList = () => {
   return fetch(`${endpoint}/list`)
     .then(response => {
-      if (!response.ok) throw new Error('Ошибка удаления')
+      if (!response.ok) throw new Error('Ошибка загрузки')
       return response.json()
     })
 }
